17c6a4624cb9f3a7398cf7036e508b7b04252dad
Jigar Brahmbhatt <findjigar@gmail.com>
2019-01-09 09:24:37 -0500

    Add restricted senders because there is way TOO MUCH spam coming!

--- app/src/main/java/jabbar/jigariyo/sms/go/SmsReceiver.kt
+++ app/src/main/java/jabbar/jigariyo/sms/go/SmsReceiver.kt
@@ -10,32 +10,42 @@ import jabbar.jigariyo.sms.go.Utils.log
 
 class SmsReceiver : BroadcastReceiver() {
 
+    private val restrictedSenders = arrayOf("BX-CENTRL", "AX-FCHRGE", "HP-KKLIFE", "MD-VirDas", "HP-TIMPTS")
+
     @SuppressLint("MissingPermission") // Permission already added at the start of the app
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Telephony.Sms.Intents.SMS_RECEIVED_ACTION) {
             val stringBuilder = StringBuilder()
             val messagesFromIntent = Telephony.Sms.Intents.getMessagesFromIntent(intent)
+            var shouldIgnoreMsg = false
             messagesFromIntent?.let {
                 for (message in messagesFromIntent) {
                     message?.let { msg ->
+                        if (restrictedSenders.contains(msg.displayOriginatingAddress)) {
+                            shouldIgnoreMsg = true
+                        }
                         stringBuilder.append("SMS from ${msg.displayOriginatingAddress}\n\n${msg.displayMessageBody}\n")
                     }
                 }
             }
-            val xxx = "\nx-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x"
-            log(xxx, stringBuilder.toString())
-            val slot = intent.getIntExtra("slot", -1)
-            val subscriptionManager =
-                context.getSystemService(SubscriptionManager::class.java) as SubscriptionManager
-            val subscriptionInfo =
-                subscriptionManager.getActiveSubscriptionInfoForSimSlotIndex(slot)
+            if (!shouldIgnoreMsg) {
+                val xxx = "\nx-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x"
+                log(xxx, stringBuilder.toString())
+                val slot = intent.getIntExtra("slot", -1)
+                val subscriptionManager =
+                    context.getSystemService(SubscriptionManager::class.java) as SubscriptionManager
+                val subscriptionInfo =
+                    subscriptionManager.getActiveSubscriptionInfoForSimSlotIndex(slot)
 
-            log(
-                "Received on slot --> $slot",
-                "Number on this slot: --> ${subscriptionInfo?.number}",
-                xxx
-            )
-            TwilioClient.sendSms(stringBuilder.toString())
+                log(
+                    "Received on slot --> $slot",
+                    "Number on this slot: --> ${subscriptionInfo?.number}",
+                    xxx
+                )
+                TwilioClient.sendSms(stringBuilder.toString())
+            } else {
+                log("Message ignored from blocked sender")
+            }
         }
     }
 }
